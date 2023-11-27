e14577e884ca6b896f896846565028fa7d98b764
Justifall <84130430+Angst04@users.noreply.github.com>
2023-11-27 20:19:25 +0300

    a1

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 let tg = window.Telegram.WebApp;
 
+tg.MainButton.show()
+tg.MainButton.text = "Отправить"; 
+
 tg.expand();
 
 tg.MainButton.textColor = "#FFFFFF";
 tg.MainButton.color = "#2cab37";
 tg.showAler("Привет!");
+tg.showConfirm("Привееет!");
 
 let item = "";

b84e6bb3b4ad6c114bb475fc977d1ced0dd6d8ef
Justifall <84130430+Angst04@users.noreply.github.com>
2023-11-28 21:31:00 +0300

    a1

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 let tg = window.Telegram.WebApp;
 
 tg.MainButton.show()
-tg.MainButton.text = "Отправить"; 
+tg.MainButton.text = "Сохранить"; 
 
 tg.expand();
 
 tg.MainButton.textColor = "#FFFFFF";
-tg.MainButton.color = "#2cab37";
-tg.showAler("Привет!");
-tg.showConfirm("Привееет!");
+tg.MainButton.color = "#2cab";
 
 let item = "";
+
+const button1 = document.getElementById('button1');
+const button2 = document.getElementById('button2');
+
+button1.addEventListener('click', () => {
+   tg.showConfirm("Вы нажали кнопку 1");
+   item = "1";
+});
+
+button2.addEventListener('click', () => {
+   tg.showAler("Вы нажали кнопку 2");
+   item = "2";
+});
+
+tg.WebApp.onEvent("mainButtonClicked", function(){
+   tg.sendData(item);
+});

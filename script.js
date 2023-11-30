4cf58ab6670c89caa389d44eb56a1efda1dc06be
Justifall <84130430+Angst04@users.noreply.github.com>
2023-11-30 10:25:01 +0300

    a2

--- script.js
+++ script.js
@@ -4,13 +4,14 @@ tg.MainButton.show();
 tg.MainButton.text = "Сохранить"; 
 
 tg.expand();
-
-tg.ready();
+console.log(tg.version());
 
 tg.MainButton.textColor = "#FFFFFF";
 tg.MainButton.color = "#2cab";
 
-let item = "";
+let item = "3";
+
+tg.ready();
 
 const button1 = document.getElementById('button1');
 const button2 = document.getElementById('button2');
@@ -18,11 +19,15 @@ const button2 = document.getElementById('button2');
 button1.addEventListener('click', () => {
    tg.showConfirm("Вы нажали кнопку 1");
    item = "1";
+   tg.sendData(item);
+   tg.close();
 });
 
 button2.addEventListener('click', () => {
    tg.showAler("Вы нажали кнопку 2");
    item = "2";
+   tg.sendData(item);
+   tg.close();
 });
 
 tg.WebApp.onEvent("mainButtonClicked", function(){

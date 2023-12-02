19f2c271a3acf37849c5e222adcee6d53a7d111f
Justifall <84130430+Angst04@users.noreply.github.com>
2023-12-02 12:46:17 +0300

    a3

--- script.js
+++ script.js
@@ -1,18 +1,39 @@
 let tg = window.Telegram.WebApp;
+const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
 
-tg.expand();
 
-tg.MainButton.show();
+let theme = document.getElementById('theme');
+if (prefersDarkMode) {
+   theme.href = 'dark-mode.css'
+   tg.MainButton.textColor = "#ffffff"; 
+   tg.MainButton.color = "#4e4e4e"; 
+} else {
+   theme.href = 'light-mode.css'
+   tg.MainButton.textColor = '#333333'; 
+   tg.MainButton.color = '#cccccc'; 
+};
 
-tg.MainButton.text = "Сохранить"; 
-tg.MainButton.textColor = "#fffb"; 
-tg.MainButton.color = "#123456"; 
 
-tg.ready();
+tg.MainButton.show()
+tg.MainButton.text = "Отправить"; 
 
-let age = 37;
+
+function outputUpdate(vol) {
+   var output = document.querySelector('#volume');
+   output.value = vol;
+
+   if (vol < 45) {
+      output.style.left = vol - 35 + 'px';
+   };
+   if (vol > 45) {
+      output.style.left = vol - 17 + 'px';
+   }
+};
+
 
 Telegram.WebApp.onEvent('mainButtonClicked', function() {
+   let age = document.getElementById("fader").value;
+
    tg.sendData(age); 
    tg.close();
 });
